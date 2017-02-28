409db109d0caf53ad63504b42e5364344ae46ba8
Daniel Keller <dkeller@imamuseum.org>
2017-02-28 11:49:22 -0500

    Moved set attribute outside of transaction

--- src/Jobs/InitializeJob.php
+++ src/Jobs/InitializeJob.php
@@ -41,12 +41,13 @@ abstract class InitializeJob extends Job
     {
         $this->displayGoal();
 
+	// Set deadlock timeout to one minute (sqlite will wait up to a minute for database lock to be released)
+        // This is the default setting with php pdo sqlite2 but with sqlite3 we must set it manually.
+        $pdo = DB::connection(config('proficio.database_connection'))->getPdo();
+        $pdo->setAttribute($pdo::ATTR_TIMEOUT, 60000);
+
         // Transaction can produce 15x faster results with Sqlite
         DB::transaction(function () {
-           // Set deadlock timeout to one minute (sqlite will wait up to a minute for database lock to be released) 
-	   // This is the default setting with php pdo sqlite2 but with sqlite3 we must set it manually. 
-	   $pdo = DB::connection(config('proficio.database_connection'))->getPdo();
-           $pdo->setAttribute($pdo::ATTR_TIMEOUT, 60000); 
 	   $this->processObjects();
         });
     }
